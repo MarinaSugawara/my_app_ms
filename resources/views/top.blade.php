23720785f300cea8a34fbbeec8551968b7001d8f
MarinaSugawara <o.towa.mari.0305.2007@gmail.com>
2023-10-09 13:51:26 +0900

    トップページ作成お試し

---
+++ resources/views/top.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <h1 class="mb-8 text-4xl md:text-6xl font-bold leading-tight tracking-tighter text-neutral-800">GhbB Ursにようこそ!</h1>
+    <p>GhbB Ursとは?</p>
+    <p>Guest House専用の掲示板(Bullet Board)です｡</p>
+
+    <h2 class="mb-4 text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">なんでも投稿OK</h2>
+
+    <p class="max-w-screen-md text-gray-500 md:text-lg">環境配慮が感じられるゲストハウスにはポイントプレゼント!</p>
+  </div>
+</div>
+
+
